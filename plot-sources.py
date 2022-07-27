b16705f9e9eb50df9b43cf3081b2d21d7e7aec2d
Rocío Mercado <rociomer@mit.edu>
2022-07-27 18:09:48 -0400

    Added CSD plots and data

--- plot-sources.py
+++ plot-sources.py
@@ -23,7 +23,7 @@ def load_pubchem_data(filename : str) -> Tuple[list, list]:
     
         return date, count
 
-def load_chembl_data(filename : str) -> Tuple[list, list]:
+def load_chembl_data(filename : str) -> Tuple[list, list, list, list, list, list]:
     with open(filename, "r") as input_data:
         # read entries; each entry is a separate line
         raw_input = input_data.read().split("\n")
@@ -42,6 +42,39 @@ def load_chembl_data(filename : str) -> Tuple[list, list]:
     
         return version, date, compounds, activities, assays, targets, documents
 
+def load_pdb_data(filename : str) -> Tuple[list, list, list]:
+    with open(filename, "r") as input_data:
+        # read entries; each entry is a separate line
+        raw_input = input_data.read().split("\n")
+    
+        # parse the date and separate it from the source count
+        date, total_entries, annual_entries = [], [], []
+        for line in raw_input[1:]:  # skip first row (header)
+            split_line = line.split(", ")
+            date.append(np.datetime64(split_line[0]))
+            total_entries.append(int(split_line[1]))
+            annual_entries.append(int(split_line[2]))
+    
+        return date, total_entries, annual_entries
+
+def load_csd_data(filename : str) -> Tuple[list, list, list]:
+    with open(filename, "r") as input_data:
+        # read entries; each entry is a separate line
+        raw_input = input_data.read().split("\n")
+    
+        # parse the date and separate it from the source count
+        date, annual_structures, ave_n_atoms_per_structure = [], [], []
+        for line in raw_input[1:]:  # skip first row (header)
+            split_line = line.split(", ")
+            date.append(np.datetime64(split_line[0]))
+            annual_structures.append(int(split_line[1]))
+            try:
+                ave_n_atoms_per_structure.append(int(split_line[2]))
+            except ValueError:
+                ave_n_atoms_per_structure.append(np.NAN)
+    
+        return date, annual_structures, ave_n_atoms_per_structure
+
 ## PubChem
 # load the PubChem data
 substance_dates, substance_sources   = load_pubchem_data(filename="./data/pubchem-substance-sources.csv")
@@ -57,7 +90,7 @@ ax.bar(x=x[::2], height=RNAi_assay_sources[::2], label=f"RNAi assays", bottom=(n
 ax.legend()
 ax.set(xlabel="Date",
        ylabel="Data sources",
-	   xticks=x[::14])
+       xticks=x[::14])
 ax.set_xticklabels(labels=substance_dates[::14], rotation=45)
 fig.tight_layout()
 fig.savefig("pubchem-sources.png")
@@ -65,18 +98,44 @@ fig.savefig("pubchem-sources.png")
 ## ChEMBL
 # load the ChEMBL data
 chembl_versions, chembl_dates, chembl_compounds, chembl_activities, chembl_assays, chembl_targets, chembl_documents = load_chembl_data(filename="./data/chembl-documents.csv")
+
 # plot the PubChem data
 fig, ax = plt.subplots(1, 1, figsize=(12,6))
 x = np.arange(len(chembl_dates))
-ax.bar(x=x, height=chembl_documents, label=f"Documents", color=palette2[3], width=1, align="edge")
-ax.legend()
+ax.bar(x=x, height=chembl_documents, color=palette2[3], width=1, align="edge")
 ax.set(xlabel="Date",
        ylabel="Documents",
-	   xticks=x[::2])
+       xticks=x[::2])
 ax.set_xticklabels(labels=chembl_dates[::2], rotation=45)
 fig.tight_layout()
 fig.savefig("chembl-documents.png")
 
 ## PDB
+# load the PDB data
+pdb_dates, pdb_total_entries, pdb_annual_entries = load_pdb_data(filename="./data/pdb-entries.csv")
+
+# plot the PDB data
+fig, ax = plt.subplots(1, 1, figsize=(12,6))
+x = np.arange(len(pdb_dates))
+ax.bar(x=x, height=pdb_total_entries, color=palette2[4], width=1, align="edge")
+ax.set(xlabel="Date",
+       ylabel="Entries",
+       xticks=x[::2])
+ax.set_xticklabels(labels=pdb_dates[::2], rotation=45)
+fig.tight_layout()
+fig.savefig("pdb-entries.png")
 
 ## CSD
+# load the CSD data
+csd_dates, csd_structures, csd_ave_n_atoms_per_structure = load_csd_data(filename="./data/csd-structures.csv")
+
+# plot the CSD data
+fig, ax = plt.subplots(1, 1, figsize=(12,6))
+x = np.arange(len(csd_dates))
+ax.bar(x=x, height=np.cumsum(csd_structures), color=palette2[5], width=1, align="edge")
+ax.set(xlabel="Date",
+       ylabel="Structures",
+       xticks=x[::2])
+ax.set_xticklabels(labels=csd_dates[::2], rotation=45)
+fig.tight_layout()
+fig.savefig("csd-structures.png")
